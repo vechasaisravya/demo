21cab5eb6f3f2614dc0694c4f31f171471317e19
vechasaisravya <53790519+vechasaisravya@users.noreply.github.com>
2019-08-07 10:54:31 +0530

    Delete test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-const express = require('express')
-const app=express()
-app.get('/sravya',(req,res)=>res.send('HELLO WORLD'))
-app.listen(4000, () => console.log(`Example app listening on port ${4000}!`))
